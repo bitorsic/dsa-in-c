b1749e894128074f08515531d6470b3e99681c33
bitorsic <bitorsic@gmail.com>
2024-11-19 23:58:09 +0530

    insertion at index in array implemented

--- arrays.c
+++ arrays.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insertAtEnd(int* arr, int* n, int element) {
-	int index = (*n);
-	
+void insertAtIndex(int* arr, int* n, int element, int index) {
+	if (index > (*n)) {
+		printf("index out of range\n");
+		return;
+	}
+
 	// base case - empty array
 	if (index == 0) {
 		*arr = element;
@@ -11,27 +14,32 @@ void insertAtEnd(int* arr, int* n, int element) {
 		return;
 	}
 
-	arr = realloc(arr, (index + 1) * sizeof(int));
-	arr[index] = element;
-
+	// allocate extra space
+	arr = realloc(arr, (*n + 1) * sizeof(int));
 	(*n)++;
+
+	// shifting elements to the right
+	for (int i = (*n) - 1; i > index; i--) {
+		arr[i] = arr[i-1];
+	}
+
+	// finally insert element at index
+	arr[index] = element;
 }
 
 int main() {
 	int n = 0;
 	int* arr = (int *) malloc(sizeof(int));
 
-	insertAtEnd(arr, &n, 2);
-	insertAtEnd(arr, &n, 3);
-	insertAtEnd(arr, &n, 4);
-	insertAtEnd(arr, &n, 5);
-	insertAtEnd(arr, &n, 6);
-	insertAtEnd(arr, &n, 7);
-	insertAtEnd(arr, &n, 8);
+	insertAtIndex(arr, &n, 2, 0);
+	insertAtIndex(arr, &n, 7, 1);
+	insertAtIndex(arr, &n, 9, 1);
+	insertAtIndex(arr, &n, 1, 1);
+	insertAtIndex(arr, &n, 5, 4);
 
 	// Traversal //
 	for (int i = 0; i < n; i++) {
-		printf("%d ", *arr + i);
+		printf("%d ", arr[i]);
 	}
 	printf("\n");
 }
